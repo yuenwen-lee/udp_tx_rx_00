ed8891edb91fd9b0ee50a9ec664012a6266bb86e
ywlee <ywlee@cisco.com>
2019-01-08 17:12:43 -0800

    modify the launch script

--- launch_udp_listener.sh
+++ launch_udp_listener.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker run -it -p 1122:1122/udp --name udp_listener --rm go_udp /bin/udp_listener
+docker run -it -p 1122:1122/udp --name udp_listener --rm go_udp /bin/udp_listener
 
